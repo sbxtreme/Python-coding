f4fdc7537f8f9c18fd32f42b702c8a312f312e59
Shobhit Bhatnagar <32269853+sbxtreme@users.noreply.github.com>
2018-02-13 23:42:47 +0530

    Update xmlparse.py

--- xmlparse.py
+++ xmlparse.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse("data.xml")
-doc = tree.getroot()
-values = doc.find('body/filAckNack/message/Message')
+root = tree.getroot()
+values = root.find('body/filAckNack/message/Message')
 print(values[2].text)
