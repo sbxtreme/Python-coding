0334631adf3b174ea782d2a54dd676249e2357e0
Shobhit Bhatnagar <sbhatnagar@sessionm.com>
2021-05-25 15:58:18 +0530

    Remove duplicated directory

--- scripts/split_json.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-with open(sys.argv[1],'r') as infile:
-    o = json.load(infile)
-    chunkSize = 1000
-    for i in range(0, len(o), chunkSize):
-        with open(sys.argv[1] + '_' + str(i//chunkSize) + '.json', 'w') as outfile:
-            json.dump(o[i:i+chunkSize], outfile)
